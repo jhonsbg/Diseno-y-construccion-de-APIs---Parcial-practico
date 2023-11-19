78623e0a2cf8b8f9b09962f8688026138fd7c988
Sebastian Bohorquez <jhonsbg@gmail.com>
2023-11-19 11:06:08 -0500

    Correction in name and route of services

--- src/supermercado/supermercado.controller.ts
+++ src/supermercado/supermercado.controller.ts
@@ -7,7 +7,7 @@ import { SupermercadoEntity } from './supermercado.entity';
 import { SupermercadoDto } from './supermercado.dto';
 import { plainToInstance } from 'class-transformer';
 
-@Controller('supermercado')
+@Controller('supermarkets')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class SupermercadoController {
     constructor(private readonly supermercadoService: SupermercadoService) {}
